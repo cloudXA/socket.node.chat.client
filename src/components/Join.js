9cb17fce76a34765456b097061b6c9c3809dcbe0
cloudXA <zming794@gmail.com>
2020-09-05 11:43:09 +0800

    初始化聊天室结构

---
+++ src/components/Join.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Join extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Join</h1>
+      </div>
+    )
+  }
+}
+
+export default Join
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// inject App component into root 
+ReactDOM.render(<App />, document.querySelector('#root'));
